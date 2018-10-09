e95152e85082483b294b9b2c4875116d2923669a
Ajhaa <atte.h.1998@gmail.com>
2018-10-09 15:49:34 +0300

    It moves!

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
-extern crate piston_window;
 extern crate nalgebra as na;
+extern crate piston_window;
 extern crate specs;
 #[macro_use]
 extern crate specs_derive;
 
+use physics::{PhysicsSystem, Position, Velocity};
 use piston_window::*;
+use specs::prelude::*;
+use specs::{Builder, DispatcherBuilder, World};
+
+type Vector2 = na::Vector2<f64>;
 
 mod physics;
 
 fn main() {
-    let mut window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
+    let mut world = World::new();
+    world.register::<Position>();
+    world.register::<Velocity>();
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(PhysicsSystem, "physics_system", &[])
+        .build();
+
+    let p1 = world
+        .create_entity()
+        .with(Position(Vector2::new(0.0, 0.0)))
+        .with(Velocity(Vector2::new(0.5, 0.0)))
+        .build();
+
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
     while let Some(event) = window.next() {
-        window.draw_2d(&event, |context, graphics| {
-            clear([1.0; 4], graphics);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      context.transform,
-                      graphics);
+        dispatcher.dispatch(&mut world.res);
+        world.maintain();
+        world.exec(|(positions,): (ReadStorage<Position>,)| {
+            (&positions).join().for_each(|pos| {
+                window.draw_2d(&event, |context, graphics| {
+                    clear([1.0; 4], graphics);
+                    rectangle(
+                        [1.0, 0.0, 0.0, 1.0], // red
+                        [pos.0.x, pos.0.y, 100.0, 100.0],
+                        context.transform,
+                        graphics,
+                    );
+                });
+            });
         });
     }
 }
--- src/physics.rs
+++ src/physics.rs
@@ -1,29 +1,23 @@
+use Vector2;
 use specs::prelude::*;
-use na::Vector2;
 
 #[derive(Component, Debug)]
 #[storage(VecStorage)]
-pub struct Position {
-    pub pos: Vector2<f32>,
-}
+pub struct Position(pub Vector2);
 
 #[derive(Component, Debug)]
 #[storage(VecStorage)]
-pub struct Velocity {
-    pub dir: Vector2<f32>,
-}
-
+pub struct Velocity(pub Vector2);
 pub struct PhysicsSystem;
 
 impl<'a> System<'a> for PhysicsSystem {
-    type SystemData = (
-        WriteStorage<'a, Position>,
-        WriteStorage<'a, Velocity>,
-    );
+    type SystemData = (WriteStorage<'a, Position>, WriteStorage<'a, Velocity>);
 
-    fn run(&mut self, (mut positions, mut velocities):Self::SystemData) {
-        (&mut positions, &mut velocities).join().for_each( |(position,velocity)| { 
-            position.pos += velocity.dir;
-         });
+    fn run(&mut self, (mut positions, mut velocities): Self::SystemData) {
+        (&mut positions, &mut velocities)
+            .join()
+            .for_each(|(position, velocity)| {
+                position.0 += velocity.0;
+            });
     }
 }
